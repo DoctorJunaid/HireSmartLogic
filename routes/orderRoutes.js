989a7dace76ff6e37ebb08a5cba4fce53ab90793
Muhammad Junaid <junaid.aurangzeb1@gmail.com>
2026-03-01 19:47:55 +0500

    feat: add order management routes for booking, status updates, chat, and completion.

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,17 +1,18 @@
 import express from "express";
 import {
+    getMyOrdersController,
+    updateOrderStatusController,
     getOrderChatController,
     sendMessageController,
     completeOrderController,
-    createBookingController
 } from "../controllers/orderControllers.js";
-import { createBookingController as directBookingController } from "../controllers/bookingControllers.js";
+import { createBookingController } from "../controllers/bookingControllers.js";
 import { verifyToken } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/my-orders", verifyToken, getMyOrdersController);
-router.post("/booking", verifyToken, directBookingController);
+router.post("/booking", verifyToken, createBookingController);
 router.patch("/:id/status", verifyToken, updateOrderStatusController);
 
 // Chat & completion routes
